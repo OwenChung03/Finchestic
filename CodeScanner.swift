c9270cc1af5be404bc6bfce91e2de36c7d88d8ff
OwenChung03 <93388699+OwenChung03@users.noreply.github.com>
2022-11-08 01:03:19 +0800

    Add files via upload

---
+++ CodeScanner.swift
@@ -0,0 +1,87 @@
+import AVFoundation
+import SwiftUI
+
+/// An enum describing the ways CodeScannerView can hit scanning problems.
+public enum ScanError: Error {
+    /// Fail in accessing the camera
+    case badInput
+    /// The camera was not capable of scanning the requested codes.
+    case badOutput
+    /// Initialization failed.
+    case initError(_ error: Error)
+    /// The camera permission is denied 
+    case permissionDenied
+}
+
+/// The result from a successful scan: the string that was scanned, and also the type of data that was found.
+/// The type is useful for times when you've asked to scan several different code types at the same time, because
+public enum ScanMode {
+    case once
+    case oncePerCode
+    case continuous
+    case manual
+}
+
+/// A SwiftUI view that is able to scan barcodes, QR codes, and more, and send back what was found.
+public struct CodeScannerView: UIViewControllerRepresentable {
+    
+    public let codeTypes: [AVMetadataObject.ObjectType]
+    public let scanMode: ScanMode
+    public let manualSelect: Bool
+    public let scanInterval: Double
+    public let showViewfinder: Bool
+    public var simulatedData = ""
+    public var shouldVibrateOnSuccess: Bool
+    public var isTorchOn: Bool
+    public var isGalleryPresented: Binding<Bool>
+    public var videoCaptureDevice: AVCaptureDevice?
+    public var completion: (Result<ScanResult, ScanError>) -> Void
+
+    public init(
+        codeTypes: [AVMetadataObject.ObjectType],
+        scanMode: ScanMode = .once,
+        manualSelect: Bool = false,
+        scanInterval: Double = 2.0,
+        showViewfinder: Bool = false,
+        simulatedData: String = "",
+        shouldVibrateOnSuccess: Bool = true,
+        isTorchOn: Bool = false,
+        isGalleryPresented: Binding<Bool> = .constant(false),
+        videoCaptureDevice: AVCaptureDevice? = AVCaptureDevice.default(for: .video),
+        completion: @escaping (Result<ScanResult, ScanError>) -> Void
+    ) {
+        self.codeTypes = codeTypes
+        self.scanMode = scanMode
+        self.manualSelect = manualSelect
+        self.showViewfinder = showViewfinder
+        self.scanInterval = scanInterval
+        self.simulatedData = simulatedData
+        self.shouldVibrateOnSuccess = shouldVibrateOnSuccess
+        self.isTorchOn = isTorchOn
+        self.isGalleryPresented = isGalleryPresented
+        self.videoCaptureDevice = videoCaptureDevice
+        self.completion = completion
+    }
+
+    public func makeUIViewController(context: Context) -> ScannerViewController {
+        return ScannerViewController(showViewfinder: showViewfinder, parentView: self)
+    }
+
+    public func updateUIViewController(_ uiViewController: ScannerViewController, context: Context) {
+        uiViewController.parentView = self
+        uiViewController.updateViewController(
+            isTorchOn: isTorchOn,
+            isGalleryPresented: isGalleryPresented.wrappedValue,
+            isManualCapture: scanMode == .manual,
+            isManualSelect: manualSelect
+        )
+    }
+    
+}
+struct CodeScannerView_Previews: PreviewProvider {
+    static var previews: some View {
+        CodeScannerView(codeTypes: [.qr]) { result in
+            // do nothing
+        }
+    }
+}
