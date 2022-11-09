df162eb7c23efae5344d0faf5e4b8c8467de16cc
OwenChung03 <93388699+OwenChung03@users.noreply.github.com>
2022-11-09 15:19:38 +0800

    Update IERC20Basic.sol

--- IERC20Basic.sol
+++ IERC20Basic.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.0;
 
 contract IERC20Basic {
-   function balanceOf(address who) {public returns (uint)};
+   function balanceOf(address who) constant public returns (uint);
    function transfer(address to, uint value) public returns (bool ok);
    event Transfer(address indexed from, address indexed to, uint value);
 }
