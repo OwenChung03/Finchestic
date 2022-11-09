c74b5f0786adcac0aeec4082372e1c58d3bcf927
OwenChung03 <93388699+OwenChung03@users.noreply.github.com>
2022-11-09 15:19:17 +0800

    Update IERC20Basic.sol

--- IERC20Basic.sol
+++ IERC20Basic.sol
@@ -1,12 +1,8 @@
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-/*
- * ERC20 interface
- * see https://github.com/ethereum/EIPs/issues/20
- * See https://github.com/ethereum/EIPs/issues/179
- */
-// contract IERC20Basic {
-//   function balanceOf(address who) {public returns (uint)};
-//   function transfer(address to, uint value) public returns (bool ok);
-//   event Transfer(address indexed from, address indexed to, uint value);
-// }
+contract IERC20Basic {
+   function balanceOf(address who) {public returns (uint)};
+   function transfer(address to, uint value) public returns (bool ok);
+   event Transfer(address indexed from, address indexed to, uint value);
+}
